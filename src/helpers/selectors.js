e4db0a000c5789c7cfea5b08b39ed3dd5432bbf9
santiago <santiago.federiconi@gmail.com>
2020-04-15 15:31:15 -0700

    adding a selector for days and the testing cases

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,3 +1,4 @@
-export function getAppointmentsForDay(state, day) {
-  
+export function getAppointmentsForDay(state, selectedDay) {
+  const matchedDay = state.days.filter(day => day.name === selectedDay);
+  return (matchedDay.length === 0 ? [] : matchedDay[0].appointments.map(appointmentID => state.appointments[appointmentID]));
 }
